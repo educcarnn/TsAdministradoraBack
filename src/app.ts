c853aadabba66fef2002ab1b8d6319511e43d800
educcarnn <educcarnn@gmail.com>
2023-08-14 13:56:43 -0300

    fix: app fix helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
+
 import registerClientes from "./routes/registerClientes";
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(helmet());
+
 
 app.use('/', registerClientes); 
 
