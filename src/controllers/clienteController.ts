ca9dabb17455d43734dd701ee2d34acd2fe79c13
educcarnn <educcarnn@gmail.com>
2023-08-17 10:32:56 -0300

    feat: uniao das tabelas

--- src/controllers/clienteController.ts
+++ src/controllers/clienteController.ts
@@ -82,21 +82,28 @@ export const obterUsuarioPorId = async (req: Request, res: Response) => {
   try {
     const client = await pool.connect();
 
-    // Consulta para obter usuário por ID
-    const query = `
+    // Consulta para obter usuário por ID de Pessoa Física
+    const queryFisica = `
       SELECT * FROM tabela_pessoas_fisicas WHERE id = $1
-      UNION ALL
+    `;
+    const resultFisica = await client.query(queryFisica, [id]);
+    const usuarioFisica = resultFisica.rows[0];
+
+    // Consulta para obter usuário por ID de Pessoa Jurídica
+    const queryJuridica = `
       SELECT * FROM tabela_pessoas_juridicas WHERE id = $1
     `;
-    const result = await client.query(query, [id]);
-
-    if (result.rowCount === 0) {
-      return res.status(404).json({ error: "Usuário não encontrado" });
-    }
-
-    const usuario = result.rows[0];
+    const resultJuridica = await client.query(queryJuridica, [id]);
+    const usuarioJuridica = resultJuridica.rows[0];
 
     client.release();
+
+    // Combinar resultados das duas consultas
+    const usuario = usuarioFisica || usuarioJuridica;
+
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
 
     res.status(200).json(usuario);
   } catch (error) {
@@ -104,3 +111,4 @@ export const obterUsuarioPorId = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Erro ao obter usuário por ID" });
   }
 };
+
