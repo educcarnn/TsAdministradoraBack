0b537c3fbc5e0faba3528d09f4b5856453973be0
educcarnn <educcarnn@gmail.com>
2023-08-24 15:26:20 -0300

    feat: new env

---
+++ src/middlewares/ValidationRoutes.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { User } from "src/entities/user";
+
+/**
+ * Middleware para garantir que um usuário tenha a role adequada para acessar uma rota
+ */
+export function ensureRole(roleRequired: string) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const user = req.user as User; // Suponha que o usuário está anexado ao objeto req após a autenticação
+
+        if (!user) {
+            return res.status(401).send("Não autenticado.");
+        }
+
+        if (user.role !== roleRequired) {
+            return res.status(403).send("Acesso negado.");
+        }
+
+        next();
+    }
+}
