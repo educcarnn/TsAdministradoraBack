8a5c3a89d8c5a201daba9e557fdda85096e51a9c
educcarnn <educcarnn@gmail.com>
2023-08-24 12:22:05 -0300

    feat new build

--- build.sh
+++ build.sh
@@ -1,33 +1,6 @@
-version: 0.2
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-phases:
-  install:
-    commands:
-      - echo "Instalando Node.js e Yarn..."
-      - curl -sL https://deb.nodesource.com/setup_14.x | bash -
-      - apt-get install -y nodejs
-      - npm install -g yarn
-      - echo "Verificando versões de Node, npm e yarn..."
-      - node -v
-      - npm -v
-      - yarn -v
-       
-  pre_build:
-    commands:
-      - echo "Instalando pacotes com Yarn..."
-      - yarn install
-      
-  build:
-    commands:
-      - echo "Compilando o projeto..."
-      - ./build.sh
-      
-  post_build:
-    commands:
-      - echo "Removendo arquivos desnecessários..."
-      - rm -rf node_modules/
-      
-artifacts:
-  base-directory: '.'
-  files:
-    - '**/*'
+yarn
+yarn build
