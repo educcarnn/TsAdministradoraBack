6911ee8100f52ac843eb9e3b824ae9af2ae1018f
educc <educcarnn@gmail.com>
2023-08-29 11:39:39 -0300

    fix app

--- src/app.ts
+++ src/app.ts
@@ -9,21 +9,18 @@ const app = express();
 
 // Habilita o CORS para permitir requisições de diferentes origens
 // Lembre-se de configurar as opções do CORS se você estiver usando cookies em domínios diferentes.
-const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'http://tsadministradora.com.br', 'http://tsadministradora.onrender.com', 'tsadministradora.onrender.com'];
+const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'http://tsadministradora.com.br', 'http://tsadministradora.onrender.com', 'http://192.168.15.147:3001'];
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin as string)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);  // Aqui está o cabeçalho Access-Control-Allow-Origin.
   }
-
-  // Note a adição de 'content-type' aqui
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
-
 // Parseia o corpo das requisições JSON para objetos JavaScript
 app.use(express.json());
 
