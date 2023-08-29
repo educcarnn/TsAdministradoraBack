6c8fa8994dfeba42ec26c56dc9b0ce0544282951
educc <educcarnn@gmail.com>
2023-08-29 10:38:30 -0300

    fix: novo acesso

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,13 @@ const app = express();
 // Lembre-se de configurar as opções do CORS se você estiver usando cookies em domínios diferentes.
 const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'http://tsadministradora.com.br', 'http://tsadministradora.onrender.com', 'http://192.168.15.147:3001'];
 
-app.use(cors({
-  origin: function(origin, callback){
-    // Se a origem da requisição estiver na lista de allowedOrigins, permita a requisição
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'A política de CORS para este site não permite o acesso a partir da origem especificada.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}));
+app.use((req, res, next) => {
 
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // Parseia o corpo das requisições JSON para objetos JavaScript
 app.use(express.json());
 
