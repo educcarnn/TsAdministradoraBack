8bd47310f2c6f198ce8942cfd12763e1bcb423ad
educc <educcarnn@gmail.com>
2023-08-29 12:11:54 -0300

    fix: auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser'; // Importa a biblioteca
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"; // Importa a biblioteca
 import routes from "./routes/routes";
 
 const app = express();
@@ -12,31 +12,32 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3001",
   "http://localhost:3000",
-  "http://www.tsadministradora.dev",
-  "https://www.tsadministradora.dev",
-  "www.tsadministradora.dev",
   "https://tsadministradora.com.br",
   "http://tsadministradora.com.br",
-  "tsadministradora.com.br"
+  "http://www.tsadministradora.dev",
+  "https://www.tsadministradora.dev",
+  "https://tsadministradora.onrender.com",
+  "http://tsadministradora.onrender.com",
 ];
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin as string)) {
-    res.header('Access-Control-Allow-Origin', origin);
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.header("Access-Control-Allow-credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
-// Parseia o corpo das requisições JSON para objetos JavaScript
-app.use(express.json());
 
+app.use(express.json());
 
 app.use(cookieParser());
 
-// Usa as rotas definidas no arquivo routes
-app.use('/', routes);
+app.use("/", routes);
 
 export default app;
