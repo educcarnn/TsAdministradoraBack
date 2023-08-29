adcd65f5fb6e424facf5a7983ecba2d21ece5ce3
educc <educcarnn@gmail.com>
2023-08-29 09:47:04 -0300

    feat: nova politica cors

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,23 +1,28 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export const isAuthenticated = (req: any, res: Response, next: NextFunction) => {
-    const token = req.cookies.token;
-  
-    if (!token) {
-      return res.status(401).json({ message: "Não autenticado." });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string, role: string };
+export const isAuthenticated = (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
 
-      req.user = decoded.userId as any;  // armazene o userId no objeto req para uso posterior
-      req.role = decoded.role as any;  // armazene a role no objeto req para uso posterior
+  if (!token) {
+    return res.status(401).json({ message: "Não autenticado." });
+  }
 
-      next();
-    } catch (error) {
-      res.status(401).json({ message: "Token inválido ou expirado." });
-    }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      userId: string;
+      role: string;
+    };
+
+    req.user = decoded.userId as any; // armazene o userId no objeto req para uso posterior
+    req.role = decoded.role as any; // armazene a role no objeto req para uso posterior
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token inválido ou expirado." });
+  }
 };
-
-
