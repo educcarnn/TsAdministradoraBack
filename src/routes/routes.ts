500edf6703745fce9c3c9b35386661a8b7fc4216
educc <educcarnn@gmail.com>
2023-08-31 13:35:37 -0300

    feat: delete user admin

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -48,7 +48,7 @@ const router = express.Router();
 router.post("/cadastrar-nova-pessoa-fisica", CadastrarPessoa);
 router.get("/obter-novas-pessoas", isAuthenticated ,isAdmin, ObterTodasPessoas);
 router.get("/pessoa/:id",  ObterPessoaPorId);
-router.delete("/pessoa-delete/:id",  DeletarPessoaPorId);
+router.delete("/pessoa-delete/:id", isAuthenticated ,isAdmin,  DeletarPessoaPorId);
 router.patch("/pessoa-patch/:id", isAuthenticated, isAdminOuUser, AtualizarPessoaPorId);
 
 //Juridica
